6faf6573d56dd65302f3d1f15b7c8d99295a474e
elmersha <user@ION>
2023-10-04 18:23:16 +0300

    only works wtih files

--- src/s21_graph.h
+++ src/s21_graph.h
@@ -2,23 +2,22 @@
 #define SRC_S21_GRAPH_H_
 #include <iostream>
 #include <cstring>
+#include <vector>
 
 namespace s21 {
-
     class s21_Graph {
         public:
         s21_Graph() = default;
         s21_Graph(int size) {}
         ~s21_Graph() = default;
-
+        std::vector<std::vector<int>> getAdjacencyMatrix();
         void LoadGraphFromFile(std::string filename);
         void ExportGraphToDot(std::string filename);
-
         private:
         int size;
-        std::vector<std::vector<int>> AdjMatrix;
-    }
+        std::vector<std::vector<int>> AdjacencyMatrix;
+    };
 
-}
+} //namespace s21
 
 #endif  // SRC_S21_GRAPH_H_
