c50fc454b4e7831cf3cb5a1729c2119eb6b51dd6
Jetta Jacks <jettajac@at-b1.msk.21-school.ru>
2023-10-06 22:45:33 +0300

    Поправила Макфайл, добавила для себя цель main и немного привела в стиль С++, добавила документацию к своему алгоритму

--- src/s21_algorithms/s21_graph_algorithms.h
+++ src/s21_algorithms/s21_graph_algorithms.h
@@ -2,10 +2,9 @@
 #define SRC_S21_ALGORITHMS_S21_GRAPH_ALGORITHMS_H_
 #include <iostream>
 #include <list>
+#include <queue>
 #include <stack>
 #include <vector>
-#include <queue>
-
 
 #include "../s21_graph/s21_graph.h"
 
@@ -13,15 +12,17 @@ namespace s21 {
 class s21_Graph;
 class GraphAlgorithms {
  public:
-  std::vector<int> DepthFirstSearch(
-      s21_Graph graph,
-      int start_vertex);  // главный метод алгоритма поиска в глубину
+  // Главный метод алгоритма поиска в глубину
+  std::vector<int> DepthFirstSearch(s21_Graph graph, int start_vertex);  
       
-  std::vector<int> BreadthFirstSearch(s21::s21_Graph graph, int start_vertex); // главный метод алгоритма поиска в ширину
+  // Главный метод алгоритма поиска в ширину
+  std::vector<int> BreadthFirstSearch( s21_Graph graph, int start_vertex); 
 
-  bool CheckVisited(std::vector<int> visited_vertices, int current_vertix) noexcept;  // метод проверки посещенных точек
-  void PrintResultOfDepthFirstSearch(
-      std::vector<int> visited_vertices) noexcept;  // метод вывода результата
+  // Метод проверки посещенных точек
+  bool CheckVisited(std::vector<int> visited_vertices,int current_vertix) noexcept;
+
+  // Метод вывода результата  
+  void PrintResultOfDepthFirstSearch(std::vector<int> visited_vertices) noexcept;  
 };
 
 }  // namespace s21
