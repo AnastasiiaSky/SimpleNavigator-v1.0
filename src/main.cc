e1c01171ddb0033f94150d75fa4d74c622dde9ff
Natalya Dyachenko <96842027+Natalya73@users.noreply.github.com>
2023-10-10 22:34:33 +0300

    Update main.cc
    main updated to deploy console menu

--- src/main.cc
+++ src/main.cc
@@ -5,8 +5,16 @@
 
 #include "./s21_algorithms/s21_graph_algorithms.h"
 #include "./s21_graph/s21_graph.h"
+#include "consolemenu.h"
 
 using namespace s21;
+
+int main() {
+    s21::ConsoleMenuInterface obj;
+    obj.Deploy();
+    return 0;
+}
+/*
 int main() {
   s21::s21_Graph graph;
   s21::GraphAlgorithms algo;
@@ -47,7 +55,7 @@ int main() {
   //   algo.PrintResultOfDepthFirstSearch(result);
   return 0;
 }
-
+*/
 // ./s21_graph/examples/graph_3_vert_simply.txt
 // ./s21_graph/examples/graph_4_vert_simply.txt
 // ./s21_graph/examples/graph_5_vert_simply.txt
