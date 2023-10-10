b2674227de4c38ba25a9e5be63e206aa6a4e3eff
JettaJac <106539528+JettaJac@users.noreply.github.com>
2023-10-10 14:09:22 +0300

    Update tests.cc

--- src/tests/tests.cc
+++ src/tests/tests.cc
@@ -5,6 +5,199 @@
 
 
 namespace s21 {
+
+    // // std::cout << "Class Graph" << std::endl;
+    
+    
+        // TEST(Graph, getAjacencyMatrix) {
+    // s21::s21_Graph graph;
+
+    //     // Ïîëó÷åíèå ìàòðèöû ñìåæíîñòè èç ãðàôà
+    //     std::vector<std::vector<int>> adjacencyMatrix = graph.getAdjacencyMatrix();
+    //     std::vector<std::vector<int>> expected {{0, 1, 1}, {1, 0, 1}, {1, 1, 0}};
+    //     //  std::vector<int> expected = {0, 1, 3, 4, 2, 5, 6};
+    //     ASSERT_EQ(adjacencyMatrix, expected);
+    // }
+
+
+    TEST(Graph, getEdgeList) {
+    s21::s21_Graph graph;
+        s21::GraphAlgorithms algo;
+        std::string InputFileName = "./examples/graph_3_vert_simply.txt";
+        graph.LoadGraphFromFile(InputFileName);
+        std::vector<std::pair<int, int>> GetedgesList = graph.getEdgesList(); 
+        std::vector<std::pair<int, int>> expected {{1, 2}, {1, 3}, {2, 1}, {2, 3}, {3, 1}, {3, 2}};
+        ASSERT_EQ(GetedgesList, expected);
+    }
+
+
+    TEST(Graph, errorOpeningFile) { // !!! Возможно тест работает неккоректно
+    s21::s21_Graph graph;
+        s21::GraphAlgorithms algo;
+        std::string InputFileName = "./examples/graph_155_vert_simply.txt";
+        graph.LoadGraphFromFile(InputFileName);
+    }
+
+
+    TEST(Graph, DOT) {
+    s21::s21_Graph graph;
+        s21::GraphAlgorithms algo;
+        std::string InputFileName = "./examples/graph_3_vert_simply.txt";
+        graph.LoadGraphFromFile(InputFileName);
+        std::string outputFile = "output.dot";
+        graph.ExportGraphToDot(outputFile);
+    }
+
+
+    TEST(Graph, Graph_DOTerror_Test) {
+    s21::s21_Graph graph;
+        s21::GraphAlgorithms algo;
+        std::string InputFileName = "./examples/graph_3_vert_simply.txt";
+        // graph.LoadGraphFromFile(InputFileName);
+        std::string outputFile = "output1.dot";
+        graph.ExportGraphToDot(outputFile);
+    }
+
+    // TEST(Graph, getAjacencyMatrix) {
+    // s21::s21_Graph graph;
+
+    //     // Ïîëó÷åíèå ìàòðèöû ñìåæíîñòè èç ãðàôà
+    //     std::vector<std::vector<int>> adjacencyMatrix = graph.getAdjacencyMatrix();
+    //     std::vector<std::vector<int>> expected {{0, 1, 1}, {1, 0, 1}, {1, 1, 0}};
+    //     //  std::vector<int> expected = {0, 1, 3, 4, 2, 5, 6};
+    //     ASSERT_EQ(adjacencyMatrix, expected);
+    // }
+
+
+    TEST(GraphTest, ExportGraphToDot_ErrorOpeningFile) {
+    // Создание объекта графа
+    s21::s21_Graph graph;
+
+    // Вызов метода ExportGraphToDot с несуществующим именем файла
+    testing::internal::CaptureStdout();
+    graph.ExportGraphToDot("DD:/nonexistent_file.dot");
+    std::string captured_stdout = testing::internal::GetCapturedStdout();
+
+    // Проверка, что сообщение об ошибке выводится в stdout
+    EXPECT_TRUE(captured_stdout.find("Error opening file") != std::string::npos);
+    }
+
+
+
+    // TEST(Graph, Graph_DOTerror_Test) {
+    // s21::s21_Graph graph;
+    //     s21::GraphAlgorithms algo;
+    //     std::string InputFileName = "./examples/graph_3_vert_simply.txt";
+    //     // graph.LoadGraphFromFile(InputFileName);
+    //     std::string outputFile = "output1.dot";
+    //     graph.ExportGraphToDot(outputFile);
+    // }
+    
+    
+    // TEST(Graph, CheckFile1) {
+    // s21::s21_Graph graph;
+    //     s21::GraphAlgorithms algo;
+    //     std::string InputFileName = "./examples/graph_2_vert_char.txt";
+    //     graph.LoadGraphFromFile(InputFileName);
+    //     bool result = graph.checkFile(InputFileName);
+    //     bool expected = false;
+    //     ASSERT_EQ(result, expected);
+    // }
+
+
+    // TEST(Graph, CheckFile2) {
+    // s21::s21_Graph graph;
+    //     s21::GraphAlgorithms algo;
+    //     std::string InputFileName = "./examples/graph_2_vert_dimNE.txt"; // graph_2_vert_R_NE_C //graph_2_vert_simply
+    //     graph.LoadGraphFromFile(InputFileName);
+    //     bool result = graph.checkFile(InputFileName);
+    //     bool expected = false;
+    //     ASSERT_EQ(result, expected);
+    // }
+
+
+    // TEST(Graph, CheckFile3) {
+    // s21::s21_Graph graph;
+    //     s21::GraphAlgorithms algo;
+    //     std::string InputFileName = "./examples/graph_2_vert_R_NE_C.txt"; //  //graph_2_vert_simply
+    //     graph.LoadGraphFromFile(InputFileName);
+    //     bool result = graph.checkFile(InputFileName);
+    //     bool expected = false;
+    //     ASSERT_EQ(result, expected);
+    // }
+
+
+    // TEST(Graph, CheckFile4) {
+    // s21::s21_Graph graph;
+    //     s21::GraphAlgorithms algo;
+    //     std::string InputFileName = "./examples/graph_2_vert_simply.txt"; //  //
+    //     graph.LoadGraphFromFile(InputFileName);
+    //     bool result = graph.checkFile(InputFileName);
+    //     bool expected = false;
+    //     ASSERT_EQ(result, expected);
+    // }
+
+
+    // TEST(Graph, CheckFile5) {
+    // s21::s21_Graph graph;
+    //     s21::GraphAlgorithms algo;
+    //     std::string InputFileName = "DDD:/graph_2_vert_simply.txt"; //  //
+    //     graph.LoadGraphFromFile(InputFileName);
+    //     bool result = graph.checkFile(InputFileName);
+    //     bool expected = false;
+    //     ASSERT_EQ(result, expected);
+    // }
+
+
+    // TEST(Graph, CheckFile6) {
+    // s21::s21_Graph graph;
+    //     s21::GraphAlgorithms algo;
+    //     std::string InputFileName = "./examples/graph_2_DIM_NOT_NUM.txt"; //  //
+    //     graph.LoadGraphFromFile(InputFileName);
+    //     bool result = graph.checkFile(InputFileName);
+    //     bool expected = false;
+    //     ASSERT_EQ(result, expected);
+    // }
+
+
+    TEST(Graph, SizeNotNumber) {
+    s21::s21_Graph graph;
+        s21::GraphAlgorithms algo;
+        // Необходимо отловить работу функции контроля правильности данных файла при вызове LoadGraphFromFile
+        testing::internal::CaptureStdout();
+        std::string InputFileName = "./examples/graph_2_DIM_NOT_NUM.txt";
+        graph.LoadGraphFromFile(InputFileName);
+        std::string captured_stdout = testing::internal::GetCapturedStdout();
+        // Проверка, что сообщение об ошибке выводится в stdout
+        EXPECT_TRUE(captured_stdout.find("Error opening file") != std::string::npos);
+    }
+
+    // TEST(Graph, DIM_NEGATIVE) {
+    // s21::s21_Graph graph;
+    //     s21::GraphAlgorithms algo;
+    //     std::string InputFileName = "./examples/graph_3_DIM_LE_ZERO.txt"; 
+    //     bool result = graph.checkFile(InputFileName);
+    //     std::cout << result << " !!!! ";
+    //     bool expected = false;
+    //     ASSERT_EQ(result, expected);
+    // }
+
+    // TEST(Graph, DIM_NEGATIVE_2) {
+    // s21::s21_Graph graph;
+    //     s21::GraphAlgorithms algo;
+    //     // отловить работу функции контроля правильности данных файла при вызове LoadGraphFromFile
+    //     testing::internal::CaptureStdout();
+    //     std::string InputFileName = "./examples/graph_2_DIM_NOT_NUM.txt";
+    //     graph.LoadGraphFromFile(InputFileName);
+    //     std::string captured_stdout = testing::internal::GetCapturedStdout();
+    //     // Проверка, что сообщение об ошибке выводится в stdout
+    //     EXPECT_TRUE(captured_stdout.find("Dimension must be > 0") != std::string::npos);
+    // }
+
+
+
+// // std::cout << "Algorithm Depth First Search" << std::endl;.
+
     TEST(Test, test_DepthFirstSearch_simple_graph_size_3) {
         s21_Graph graph;
         s21::GraphAlgorithms algo;
@@ -46,6 +239,7 @@ namespace s21 {
         ASSERT_ANY_THROW(visited_vertices = algo.DepthFirstSearch(graph, start));
     }
 
+
     TEST(Test, test_DepthFirstSearch_simple_graph_size_13) {
         s21_Graph graph;
         s21::GraphAlgorithms algo;
@@ -61,7 +255,6 @@ namespace s21 {
         for(int it = 0; it < result.size(); ++it) {
             ASSERT_EQ(visited_vertices[it], result[it]);
         }
-
 
         start = 9;
         visited_vertices.clear();
@@ -82,6 +275,7 @@ namespace s21 {
         ASSERT_ANY_THROW(visited_vertices = algo.DepthFirstSearch(graph, start));
 
     }
+
 
     TEST(Test, test_DepthFirstSearch_directed_graph_8_vert) {
         s21_Graph graph;
@@ -106,10 +300,10 @@ namespace s21 {
         for(int it = 0; it < result.size(); ++it) {
             ASSERT_EQ(visited_vertices[it], result[it]);
         }
-
     }
 
-        TEST(Test, test_DepthFirstSearch_directed_graph_21_vert) {
+
+    TEST(Test, test_DepthFirstSearch_directed_graph_21_vert) {
         s21_Graph graph;
         s21::GraphAlgorithms algo;
         std::string InputFileName = "./examples/graph_21_vert_direct.txt";
@@ -242,6 +436,7 @@ namespace s21 {
         }
     }
 
+
 // // std::cout << "Algorithm Breadth First Search" << std::endl;
 
     TEST(Test, test_BreadFirstSearch_simple_graph_size_3) {
@@ -289,6 +484,7 @@ namespace s21 {
         ASSERT_ANY_THROW(visited_vertices = algo.BreadthFirstSearch(graph, start));
     }
 
+
     TEST(Test, test_BreadFirstSearch_simple_graph_size_13) {
         s21_Graph graph;
         s21::GraphAlgorithms algo;
@@ -324,6 +520,7 @@ namespace s21 {
         ASSERT_ANY_THROW(visited_vertices = algo.BreadthFirstSearch(graph, start));
     }
 
+
     TEST(Test, test_BreadFirstSearch_directed_graph_8_vert) {
         s21_Graph graph;
         s21::GraphAlgorithms algo;
@@ -350,6 +547,7 @@ namespace s21 {
         }
 
     }
+
 
     TEST(Test, test_BreadFirstSearch_directed_graph_21_vert) {
         s21_Graph graph;
@@ -415,6 +613,7 @@ namespace s21 {
         }
     }
 
+
     TEST(Test, FloidWarshall_graph_3_vert_simply) {
         s21_Graph graph;
         s21::GraphAlgorithms algo;
@@ -424,6 +623,7 @@ namespace s21 {
         std::vector<std::vector<int>> result {{0, 2, 3, 3}, {2, 0, 5, 1}, {3, 5, 0, 6}, {3, 1, 6, 0}};
         ASSERT_EQ(minimum_distance, result);
     }
+
 
     TEST(Test, FloidWarshall_weighted_directed_graph_8_vert) {
         // s21_Graph graph;
@@ -444,6 +644,7 @@ namespace s21 {
         // ASSERT_EQ(minimum_distance, result);
     }
 
+
     TEST(Test, FloidWarshall_weighted_directed_graph_4_vert) {
         s21_Graph graph;
         s21::GraphAlgorithms algo;
@@ -459,6 +660,7 @@ namespace s21 {
         ASSERT_EQ(minimum_distance, result);
     }
      
+
      TEST(Test, FloidWarshall_weighted_directed_graph_3_vert) {
         s21_Graph graph;
         s21::GraphAlgorithms algo;
@@ -472,152 +674,6 @@ namespace s21 {
         }; 
         ASSERT_EQ(minimum_distance, result);
     }
-
-    // TEST(Graph, getAjacencyMatrix) {
-    // s21::s21_Graph graph;
-
-    //     // Ïîëó÷åíèå ìàòðèöû ñìåæíîñòè èç ãðàôà
-    //     std::vector<std::vector<int>> adjacencyMatrix = graph.getAdjacencyMatrix();
-    //     std::vector<std::vector<int>> expected {{0, 1, 1}, {1, 0, 1}, {1, 1, 0}};
-    //     //  std::vector<int> expected = {0, 1, 3, 4, 2, 5, 6};
-    //     ASSERT_EQ(adjacencyMatrix, expected);
-    // }
-
-    TEST(Graph, getEdgeList) {
-    s21::s21_Graph graph;
-        s21::GraphAlgorithms algo;
-        std::string InputFileName = "./examples/graph_3_vert_simply.txt";
-        graph.LoadGraphFromFile(InputFileName);
-        std::vector<std::pair<int, int>> GetedgesList = graph.getEdgesList(); 
-        std::vector<std::pair<int, int>> expected {{1, 2}, {1, 3}, {2, 1}, {2, 3}, {3, 1}, {3, 2}};
-        ASSERT_EQ(GetedgesList, expected);
-    }
-    TEST(GraphTest, ExportGraphToDot_ErrorOpeningFile) {
-    // Создание объекта графа
-    s21::s21_Graph graph;
-
-    // Вызов метода ExportGraphToDot с несуществующим именем файла
-    testing::internal::CaptureStdout();
-    graph.ExportGraphToDot("DD:/nonexistent_file.dot");
-    std::string captured_stdout = testing::internal::GetCapturedStdout();
-
-    // Проверка, что сообщение об ошибке выводится в stdout
-    EXPECT_TRUE(captured_stdout.find("Error opening file") != std::string::npos);
-}
-    TEST(Graph, errorOpeningFile) { // !!! Возможно тест работает неккоректно
-    s21::s21_Graph graph;
-        s21::GraphAlgorithms algo;
-        std::string InputFileName = "./examples/graph_155_vert_simply.txt";
-        graph.LoadGraphFromFile(InputFileName);
-    }
-
-    TEST(Graph, DOT) {
-    s21::s21_Graph graph;
-        s21::GraphAlgorithms algo;
-        std::string InputFileName = "./examples/graph_3_vert_simply.txt";
-        graph.LoadGraphFromFile(InputFileName);
-        std::string outputFile = "output.dot";
-        graph.ExportGraphToDot(outputFile);
-    }
-
-    // TEST(Graph, Graph_DOTerror_Test) {
-    // s21::s21_Graph graph;
-    //     s21::GraphAlgorithms algo;
-    //     std::string InputFileName = "./examples/graph_3_vert_simply.txt";
-    //     // graph.LoadGraphFromFile(InputFileName);
-    //     std::string outputFile = "output1.dot";
-    //     graph.ExportGraphToDot(outputFile);
-    // }
-    TEST(Graph, CheckFile1) {
-s21::s21_Graph graph;
-    s21::GraphAlgorithms algo;
-    std::string InputFileName = "./examples/graph_2_vert_char.txt";
-    graph.LoadGraphFromFile(InputFileName);
-    bool result = graph.checkFile(InputFileName);
-    bool expected = false;
-    ASSERT_EQ(result, expected);
-}
-TEST(Graph, CheckFile2) {
-s21::s21_Graph graph;
-    s21::GraphAlgorithms algo;
-    std::string InputFileName = "./examples/graph_2_vert_dimNE.txt"; // graph_2_vert_R_NE_C //graph_2_vert_simply
-    graph.LoadGraphFromFile(InputFileName);
-    bool result = graph.checkFile(InputFileName);
-    bool expected = false;
-    ASSERT_EQ(result, expected);
-}
-TEST(Graph, CheckFile3) {
-s21::s21_Graph graph;
-    s21::GraphAlgorithms algo;
-    std::string InputFileName = "./examples/graph_2_vert_R_NE_C.txt"; //  //graph_2_vert_simply
-    graph.LoadGraphFromFile(InputFileName);
-    bool result = graph.checkFile(InputFileName);
-    bool expected = false;
-    ASSERT_EQ(result, expected);
-}
-TEST(Graph, CheckFile4) {
-s21::s21_Graph graph;
-    s21::GraphAlgorithms algo;
-    std::string InputFileName = "./examples/graph_2_vert_simply.txt"; //  //
-    graph.LoadGraphFromFile(InputFileName);
-    bool result = graph.checkFile(InputFileName);
-    bool expected = false;
-    ASSERT_EQ(result, expected);
-}
-TEST(Graph, CheckFile5) {
-s21::s21_Graph graph;
-    s21::GraphAlgorithms algo;
-    std::string InputFileName = "DDD:/graph_2_vert_simply.txt"; //  //
-    graph.LoadGraphFromFile(InputFileName);
-    bool result = graph.checkFile(InputFileName);
-    bool expected = false;
-    ASSERT_EQ(result, expected);
-}
-
-TEST(Graph, CheckFile6) {
-s21::s21_Graph graph;
-    s21::GraphAlgorithms algo;
-    std::string InputFileName = "./examples/graph_2_DIM_NOT_NUM.txt"; //  //
-    graph.LoadGraphFromFile(InputFileName);
-    bool result = graph.checkFile(InputFileName);
-    bool expected = false;
-    ASSERT_EQ(result, expected);
-}
-
-TEST(Graph, SizeNotNumber) {
-s21::s21_Graph graph;
-    s21::GraphAlgorithms algo;
-      // Необходимо отловить работу функции контроля правильности данных файла при вызове LoadGraphFromFile
-    testing::internal::CaptureStdout();
-    std::string InputFileName = "./examples/graph_2_DIM_NOT_NUM.txt";
-     graph.LoadGraphFromFile(InputFileName);
-     std::string captured_stdout = testing::internal::GetCapturedStdout();
-      // Проверка, что сообщение об ошибке выводится в stdout
-    EXPECT_TRUE(captured_stdout.find("Error opening file") != std::string::npos);
-}
-
-TEST(Graph, DIM_NEGATIVE) {
-s21::s21_Graph graph;
-    s21::GraphAlgorithms algo;
-    std::string InputFileName = "./examples/graph_3_DIM_LE_ZERO.txt"; 
-    bool result = graph.checkFile(InputFileName);
-    std::cout << result << " !!!! ";
-    bool expected = false;
-    ASSERT_EQ(result, expected);
-}
-
-TEST(Graph, DIM_NEGATIVE_2) {
-s21::s21_Graph graph;
-    s21::GraphAlgorithms algo;
-      // отловить работу функции контроля правильности данных файла при вызове LoadGraphFromFile
-    testing::internal::CaptureStdout();
-    std::string InputFileName = "./examples/graph_2_DIM_NOT_NUM.txt";
-     graph.LoadGraphFromFile(InputFileName);
-     std::string captured_stdout = testing::internal::GetCapturedStdout();
-      // Проверка, что сообщение об ошибке выводится в stdout
-    EXPECT_TRUE(captured_stdout.find("Dimension must be > 0") != std::string::npos);
-}
-
 
 
 int main(int argc, char** argv) {
