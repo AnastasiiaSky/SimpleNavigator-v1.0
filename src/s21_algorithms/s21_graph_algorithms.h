3267f9079ee7574c17be0e6e0c7d49ba5947289f
olga <morfinpo@student.21-school.ru>
2023-10-11 20:50:11 +0300

    ;

--- src/s21_algorithms/s21_graph_algorithms.h
+++ src/s21_algorithms/s21_graph_algorithms.h
@@ -1,12 +1,12 @@
 #ifndef SRC_S21_ALGORITHMS_S21_GRAPH_ALGORITHMS_H_
 #define SRC_S21_ALGORITHMS_S21_GRAPH_ALGORITHMS_H_
 
-#include <algorithm> // !!! возможно не надо
+#include <algorithm>  // !!! возможно не надо
 #include <iostream>
-#include <list> /// Заменить на самописные
-#include <queue> /// Заменить на самописные
-#include <stack> /// Заменить на самописные
-#include <vector> /// Заменить на самописные
+#include <list>    /// Заменить на самописные
+#include <queue>   /// Заменить на самописные
+#include <stack>   /// Заменить на самописные
+#include <vector>  /// Заменить на самописные
 // #include <limits> // !!! возможно не надо
 // #include <limits.h> // !!! возможно не надо
 
@@ -14,44 +14,45 @@
 
 namespace s21 {
 
-  struct TsmResult {
-    int* vertices;    // массив с искомым маршрутом (с порядком обхода вершин). Вместо int* можно использовать std::vector<int>
-    double distance;  // длина этого маршрута
-  };
+struct TsmResult {
+  int *vertices;  // массив с искомым маршрутом (с порядком обхода вершин).
+                  // Вместо int* можно использовать std::vector<int>
+  double distance;  // длина этого маршрута
+};
 
 class s21_Graph;
 class GraphAlgorithms {
  public:
-
-  const int inf = std::numeric_limits<int>::max();;  // !!! Перенести в класс, наверное
-
-
+  const int inf =
+      std::numeric_limits<int>::max();  // !!! Перенести в класс, наверное
 
   // Главный метод алгоритма поиска в глубину
-  std::vector<int> DepthFirstSearch(s21_Graph graph, int start_vertex);  
-      
+  std::vector<int> DepthFirstSearch(s21_Graph graph, int start_vertex);
+
   // Главный метод алгоритма поиска в ширину
-  std::vector<int> BreadthFirstSearch(s21_Graph &graph, int start_vertex); 
+  std::vector<int> BreadthFirstSearch(s21_Graph &graph, int start_vertex);
 
   // Метод проверки посещенных точек
-  bool CheckVisited(std::vector<int> visited_vertices,int current_vertix) noexcept;
+  bool CheckVisited(std::vector<int> visited_vertices,
+                    int current_vertix) noexcept;
 
   // Дейкстра
   int GetShortestPathBetweenVertices(s21_Graph &graph, int vertex1,
                                      int vertex2);
 
   // Метод поиска пути по алгоритму Флойда Уошера
-  std::vector<std::vector<int>> GetShortestPathsBetweenAllVertices(s21_Graph &graph);
-
+  std::vector<std::vector<int>> GetShortestPathsBetweenAllVertices(
+      s21_Graph &graph);
 
   TsmResult SolveTravelingSalesmanProblem(s21_Graph &graph);
 
-  // Метод вывода результата пути 
-  void PrintResultOfDepthFirstSearch(std::vector<int> visited_vertices) noexcept;  
+  // Метод вывода результата пути
+  void PrintResultOfDepthFirstSearch(
+      std::vector<int> visited_vertices) noexcept;
 
   // Метод ввывода матрицы смежности
-  void PrintAdjacencyMatrix(std::vector<std::vector<int>> adjacency_matrix) noexcept;
-
+  void PrintAdjacencyMatrix(
+      std::vector<std::vector<int>> adjacency_matrix) noexcept;
 };
 
 }  // namespace s21
