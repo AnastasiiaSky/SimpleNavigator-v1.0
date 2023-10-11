e24f3abe2a916434f3bc2f831a210876d9e0e71d
Jetta Jacks <jettajac@at-b1.msk.21-school.ru>
2023-10-11 23:30:41 +0300

    Написала второстепенные функции

--- src/s21_algorithms/s21_graph_algorithms.h
+++ src/s21_algorithms/s21_graph_algorithms.h
@@ -7,6 +7,8 @@
 #include <queue> /// Заменить на самописные
 #include <stack> /// Заменить на самописные
 #include <vector> /// Заменить на самописные
+#include <math.h>
+#include <random> 
 // #include <limits> // !!! возможно не надо
 // #include <limits.h> // !!! возможно не надо
 
@@ -15,7 +17,7 @@
 namespace s21 {
 
   struct TsmResult {
-    int* vertices;    // массив с искомым маршрутом (с порядком обхода вершин). Вместо int* можно использовать std::vector<int>
+    int *vertices;    // массив с искомым маршрутом (с порядком обхода вершин). Вместо int* можно использовать std::vector<int>
     double distance;  // длина этого маршрута
   };
 
@@ -28,13 +30,13 @@ class GraphAlgorithms {
 
 
   // Главный метод алгоритма поиска в глубину
-  std::vector<int> DepthFirstSearch(s21_Graph graph, int start_vertex);  
+  std::vector<int> DepthFirstSearch(s21_Graph &graph, int start_vertex);  
       
   // Главный метод алгоритма поиска в ширину
   std::vector<int> BreadthFirstSearch(s21_Graph &graph, int start_vertex); 
 
   // Метод проверки посещенных точек
-  bool CheckVisited(std::vector<int> visited_vertices,int current_vertix) noexcept;
+  bool CheckVisited(std::vector<int> visited_vertices, int current_vertix) noexcept;
 
   // Метод поиска пути по алгоритму Флойда Уошера
   std::vector<std::vector<int>> GetShortestPathsBetweenAllVertices(s21_Graph &graph);
@@ -47,6 +49,22 @@ class GraphAlgorithms {
 
   // Метод ввывода матрицы смежности
   void PrintAdjacencyMatrix(std::vector<std::vector<int>> adjacency_matrix) noexcept;
+
+  private:
+    // Рассчитывает вероятность прохождения по текущему ребру
+    void CreateProbabilityMatrix (std::vector<std::vector<double>> &pobability_list,
+    std::vector<std::vector<double>> pheramone_matrix, std::vector<std::vector<int>> matrix_adjacency);
+
+    // Пересчитывает количество ферамонта на текущему ребре
+    void RecalculatePheramoneMatrix (std::vector<std::vector<double>> &pheramone_matrix, int distance);
+
+    // Генерируем рандомное число в заданном диапозоне
+    int random(int min, int max) const;
+
+    // Выбираем рандомно вершину, в которую идем 
+    int SelectNextVertex (list<double> probability_list);
+
+    
 
 };
 
