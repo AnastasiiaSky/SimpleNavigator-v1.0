2f60b8f5831c371a983547f95dd5c4b06a0f16a2
Olga <97388627+dub-del@users.noreply.github.com>
2023-10-11 19:55:46 +0300

    Update tests.cc - add dijksra tests

--- src/tests/tests.cc
+++ src/tests/tests.cc
@@ -613,6 +613,66 @@ namespace s21 {
         }
     }
 
+TEST(GraphAlgorithmsTest, ShortestPathCase1) {
+  s21::s21_Graph graph(5);
+  s21::GraphAlgorithms algo;
+  std::string InputFileName = "./examples/graph_5_for_dijkstra.txt";
+  graph.LoadGraphFromFile(InputFileName);
+
+  int shortestDistance = algo.GetShortestPathBetweenVertices(graph, 1, 4);
+  EXPECT_EQ(shortestDistance, 14);
+}
+
+TEST(GraphAlgorithmsTest, ShortestPathCase2) {
+  s21::s21_Graph graph(5);
+  s21::GraphAlgorithms algo;
+  std::string InputFileName = "./examples/graph_5_for_dijkstra.txt";
+  graph.LoadGraphFromFile(InputFileName);
+
+  int shortestDistance = algo.GetShortestPathBetweenVertices(graph, 1, 3);
+  EXPECT_EQ(shortestDistance, 10);
+}
+
+TEST(GraphAlgorithmsTest, ShortestPathCase3) {
+  s21::s21_Graph graph(5);
+  s21::GraphAlgorithms algo;
+  std::string InputFileName = "./examples/graph_4_for_dijkstra.txt";
+  graph.LoadGraphFromFile(InputFileName);
+
+  int shortestDistance = algo.GetShortestPathBetweenVertices(graph, 1, 3);
+  EXPECT_EQ(shortestDistance, 8);
+}
+
+TEST(GraphAlgorithmsTest, ShortestPathCase4) {
+  s21::s21_Graph graph(5);
+  s21::GraphAlgorithms algo;
+  std::string InputFileName = "./examples/graph_4_for_dijkstra.txt";
+  graph.LoadGraphFromFile(InputFileName);
+
+  int shortestDistance = algo.GetShortestPathBetweenVertices(graph, 5, 2);
+  EXPECT_EQ(shortestDistance, 5);
+}
+
+TEST(GraphAlgorithmsTest, InvalidVertices) {
+  s21::s21_Graph graph(5);
+  s21::GraphAlgorithms algo;
+  std::string InputFileName = "./examples/graph_5_for_dijkstra.txt";
+  graph.LoadGraphFromFile(InputFileName);
+
+  EXPECT_THROW(algo.GetShortestPathBetweenVertices(graph, 1, 6),
+               std::invalid_argument);
+}
+
+TEST(GraphAlgorithmsTest, InvalidVertices1) {
+  s21::s21_Graph graph(5);
+  s21::GraphAlgorithms algo;
+  std::string InputFileName = "./examples/graph_5_for_dijkstra.txt";
+  graph.LoadGraphFromFile(InputFileName);
+
+  EXPECT_THROW(algo.GetShortestPathBetweenVertices(graph, 0, 4),
+               std::invalid_argument);
+}
+
 
     TEST(Test, FloidWarshall_graph_3_vert_simply) {
         s21_Graph graph;
