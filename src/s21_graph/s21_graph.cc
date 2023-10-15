a4acf5daaacc267361c1dd7bb9c2acd9e1517cc6
Elmer Shadout <elmersha@il-j6.msk.21-school.ru>
2023-10-15 18:23:29 +0300

    exceptions added and exta comments deleted

--- src/s21_graph/s21_graph.cc
+++ src/s21_graph/s21_graph.cc
@@ -248,42 +248,12 @@ void s21::s21_Graph::LoadGraphFromFile(std::string filename) {
 }
 
 
-// void s21::s21_Graph::LoadGraphFromFile(std::string filename) {
-//   std::ifstream ifs(filename);
-//   if (!ifs.is_open()) {
-//     std::cout << "Error opening file";
-//     return;
-//   }
-//   if(!CheckFile(filename)) {std::cout << "Error opening file";}
-//   ifs >> size_;  // read the graph size from file
-//   if(size_ <= 0) { std::cout << "Dimension must be > 0"; return;}
-//   AdjacencyMatrix_.resize(size_,
-//                           std::vector<int>(size_));  // change the matrix size
-//   for (int i = 0; i < size_; ++i) {
-//     for (int j = 0; j < size_; ++j) {
-//       ifs >>  AdjacencyMatrix_[i][j];  // take the values from the files to the  matrix
-//     }
-//   }
-//     // s21_Graph temp;
-//     // for (int i = 0; i < size_; ++i) {
-//     //   for (int j = 0; j < size_; ++j) {
-//     //   temp.AdjacencyMatrix_[i][j] = this->AdjacencyMatrix_[i][j];  
-//     //   if(temp.AdjacencyMatrix_[i][j] != temp.AdjacencyMatrix_[j][i]) {
-//     //     temp.AdjacencyMatrix_[j][i] = temp.AdjacencyMatrix_[i][j];
-//     //   }
-//     //        std::vector<int> visited = this->FindPath(temp, 0);
-//     //        for (int isVisited : visited) {
-//     //           if (isVisited == 0) {
-//     //         std::cout << "Graph is not connected";;  // ���� �� �������
-//     //     }
-//     // }
 
-
-//     // }
-//   // }
-//   ifs.close();
-// }
-
+/// @brief Метод, специально встроенный для определения связности графа   
+/// @param graph - текущий граф.
+/// @param start_vertex - точка начала происка пути.
+/// @return std::vector<int> visited_vertices - результатом работы метода
+/// является вектор посещенных точек
 std::vector<int> s21::s21_Graph::FindPath(s21_Graph &graph, int start_vertex) {
   // if (start_vertex >= tmp.size() || start_vertex < 0) {
   //   throw std::out_of_range("Start vertex is out of range");
@@ -315,7 +285,9 @@ std::vector<int> s21::s21_Graph::FindPath(s21_Graph &graph, int start_vertex) {
   }
   return visited_vertices;
 }
-
+/// @brief Метод, меняющий матрицу смежности ориентированного графа на матрицу смежности неориентированного
+/// @param graph - текущий граф.
+/// @return ничего не возвращает
 void s21::s21_Graph::MakeMatrixUndirected(s21::s21_Graph &graph)
 {
   for(int i = 0; i < size_/2; ++i) {
@@ -324,7 +296,9 @@ void s21::s21_Graph::MakeMatrixUndirected(s21::s21_Graph &graph)
     }
   }
 }
-
+/// @brief Метод, определяющий, является ли граф направленным  
+/// @param std::vector<std::vector<int>> AdjacencyMatrix - матрица смежности графа
+/// @return возвращает либо false либо true
 bool s21::s21_Graph::IsDerrected(std::vector<std::vector<int>> AdjacencyMatrix)
 {
   for(int i = 0; i < size_; ++i) {
@@ -333,12 +307,14 @@ bool s21::s21_Graph::IsDerrected(std::vector<std::vector<int>> AdjacencyMatrix)
       return true;
     }
   }
-
   }
   return false;
 }
 
-
+/// @brief Метод, определяющий, является ли граф связным  
+/// @param  graph - текущий граф.
+/// @param filename - файл, из которого приходит матрица
+/// @return возвращает либо false либо true
 bool s21::s21_Graph::IsGraphConnected(s21_Graph &graph, std::string filename)  {
   if(IsDerrected(graph.AdjacencyMatrix_) == false) {
     std::vector<int> visited = FindPath(graph, 3);
@@ -357,227 +333,6 @@ bool s21::s21_Graph::IsGraphConnected(s21_Graph &graph, std::string filename)  {
   }
   return false;  // 
 }
-
-
-// std::vector<int> s21::s21_Graph::FindPath(s21_Graph &graph,
-//                                                         int start_vertex) {
-//   if (start_vertex > graph.get_graph_size() || start_vertex < 0) {
-//     throw std::length_error("Start vertex is incorrect");
-//   }
-//   std::vector<int> visited_vertices;
-//   std::stack<int> vertex_stack;
-//   std::vector<std::vector<int>> adjacency_list = graph.getAdjacencyList();
-
-//   vertex_stack.push(start_vertex);
-
-//   while (!vertex_stack.empty()) {
-//     int current_vertex = vertex_stack.top();
-//     vertex_stack.pop();
-//      if(find(visited_vertices.begin(), visited_vertices.end(), current_vertex) !=visited_vertices.end()) {
-//       continue;
-//     }
-
-//     std::vector<int> adjacent_vertices = adjacency_list[current_vertex - 1];
-//     visited_vertices.push_back(current_vertex);
-
-//     for (int it = adjacent_vertices.size() - 1; it >= 0; --it) {
-//       if (!(find(visited_vertices.begin(), visited_vertices.end(), adjacent_vertices[it]) !=visited_vertices.end()))
-//       {
-//         vertex_stack.push(adjacent_vertices[it]);
-//       }
-//     }
-//   }
-//   return visited_vertices;
-// }
- 
-// bool s21::s21_Graph::IsGraphConnected() const {
-//   s21::s21_Graph result;
-//     for(int i = //     for (int j = 0; j < size_; j++) {
-//       if(result.AdjacencyMatrix_[i][j] !=result.AdjacencyMatrix_[j][i]) {
-//         result.AdjacencyMatrix_[j][i] = result.AdjacencyMatrix_[i][j];
-//       } 
-//     }
-//   }0; i < size_; i++) {
-
-//   std::vector<int> visited(size_, 0);
-//   visited = result.FindPath(result, 0);
-//     // ��������� ����� � ������� �� ������ �������
-//     // ���������, ���� �� ��� ������� ��������
-//     for (int isVisited : visited) {
-//         if (isVisited == 0) {
-//             return false;  // ���� �� �������
-//         }
-//     }
-//     return true;  // ���� �������
-// }
-
-
-// void s21::s21_Graph::LoadGraphFromFile(std::string filename) {
-//   std::ifstream ifs(filename);
-//   if (!ifs.is_open()) {
-//     std::cout << "Error opening file";
-//     return;
-//   }
-//   if(!CheckFile(filename)) {std::cout << "Error opening file";}
-//   ifs >> size_;  // read the graph size from file
-//   if(size_ <= 0) { std::cout << "Dimension must be > 0"; return;}
-//   AdjacencyMatrix_.resize(size_,
-//                           std::vector<int>(size_));  // change the matrix size
-//   for (int i = 0; i < size_; ++i) {
-//     for (int j = 0; j < size_; ++j) {
-//       ifs >>  AdjacencyMatrix_[i][j];  // take the values from the files to the  matrix
-//     }
-//   }
-//     // s21_Graph temp;
-//     // for (int i = 0; i < size_; ++i) {
-//     //   for (int j = 0; j < size_; ++j) {
-//     //   temp.AdjacencyMatrix_[i][j] = this->AdjacencyMatrix_[i][j];  
-//     //   if(temp.AdjacencyMatrix_[i][j] != temp.AdjacencyMatrix_[j][i]) {
-//     //     temp.AdjacencyMatrix_[j][i] = temp.AdjacencyMatrix_[i][j];
-//     //   }
-//     //        std::vector<int> visited = this->FindPath(temp, 0);
-//     //        for (int isVisited : visited) {
-//     //           if (isVisited == 0) {
-//     //         std::cout << "Graph is not connected";;  // ���� �� �������
-//     //     }
-//     // }
-
-
-//     // }
-//   // }
-//   ifs.close();
-// }
-
-// std::vector<int> s21::s21_Graph::FindPath(s21_Graph &graph, int start_vertex) {
-//   // if (start_vertex >= tmp.size() || start_vertex < 0) {
-//   //   throw std::out_of_range("Start vertex is out of range");
-//   // }
-  
-//   std::vector<int> visited_vertices;
-//   std::stack<int> vertex_stack;
-//   std::vector<std::vector<int>> adjacency_list = graph.getAdjacencyList();
-
-//   vertex_stack.push(start_vertex);
-
-//   while (!vertex_stack.empty()) {
-//     int current_vertex = vertex_stack.top();
-//     vertex_stack.pop();
-
-//     if (std::find(visited_vertices.begin(), visited_vertices.end(), current_vertex) != visited_vertices.end()) {
-//       continue;
-//     }
-
-//     visited_vertices.push_back(current_vertex);
-
-//     std::vector<int> adjacent_vertices = adjacency_list[current_vertex - 1];
-//     for (int it = adjacent_vertices.size() - 1; it >= 0; --it) {
-//       int adjacent_vertex = adjacent_vertices[it];
-//       if (std::find(visited_vertices.begin(), visited_vertices.end(), adjacent_vertex) == visited_vertices.end()) {
-//         vertex_stack.push(adjacent_vertex);
-//       }
-//     }
-//   }
-//   return visited_vertices;
-// }
-
-// void s21::s21_Graph::MakeMatrixUndirected(s21::s21_Graph &graph)
-// {
-//   for(int i = 0; i < size_/2; ++i) {
-//     for(int j = 0; j < size_/2; ++j) {
-//       this->AdjacencyMatrix_[j][i] = this->AdjacencyMatrix_[i][j];
-//       // if(AdjacencyMatrix[i][j] != AdjacencyMatrix[j][i]) {
-//       //   return true;
-//       // }
-//     }
-//   }
-// }
-
-// bool s21::s21_Graph::IsDerrected(std::vector<std::vector<int>> AdjacencyMatrix)
-// {
-//   for(int i = 0; i < size_; ++i) {
-//   for(int j = 0; j < size_; ++j) {
-//     if(AdjacencyMatrix[i][j] != AdjacencyMatrix[j][i]) {
-//       return true;
-//     }
-//   }
-
-//   }
-//   return false;
-// }
-
-
-// bool s21::s21_Graph::IsGraphConnected(s21_Graph &graph, std::string filename)  {
-//   if(IsDerrected(graph.AdjacencyMatrix_) == false) {
-//     std::vector<int> visited = FindPath(graph, 3);
-//     if(visited.size() == size_) {
-//       std::cout << " graph connected ";
-//       return true;
-//     } else {std::cout << " graph NOT connected ";}
-//   } else {
-//     MakeMatrixUndirected(graph);
-//     std::vector<int> visited = FindPath(graph, 3);
-//     if(visited.size() == size_) {
-//       std::cout << " graph connected ";
-//       graph.LoadGraphFromFile(filename);
-//       return true;
-//     } else {std::cout << " graph NOT connected ";}
-//   }
-//   return false;  // 
-// }
-
-
-// std::vector<int> s21::s21_Graph::FindPath(s21_Graph &graph,
-//                                                         int start_vertex) {
-//   if (start_vertex > graph.get_graph_size() || start_vertex < 0) {
-//     throw std::length_error("Start vertex is incorrect");
-//   }
-//   std::vector<int> visited_vertices;
-//   std::stack<int> vertex_stack;
-//   std::vector<std::vector<int>> adjacency_list = graph.getAdjacencyList();
-
-//   vertex_stack.push(start_vertex);
-
-//   while (!vertex_stack.empty()) {
-//     int current_vertex = vertex_stack.top();
-//     vertex_stack.pop();
-//      if(find(visited_vertices.begin(), visited_vertices.end(), current_vertex) !=visited_vertices.end()) {
-//       continue;
-//     }
-
-//     std::vector<int> adjacent_vertices = adjacency_list[current_vertex - 1];
-//     visited_vertices.push_back(current_vertex);
-
-//     for (int it = adjacent_vertices.size() - 1; it >= 0; --it) {
-//       if (!(find(visited_vertices.begin(), visited_vertices.end(), adjacent_vertices[it]) !=visited_vertices.end()))
-//       {
-//         vertex_stack.push(adjacent_vertices[it]);
-//       }
-//     }
-//   }
-//   return visited_vertices;
-// }
- 
-// bool s21::s21_Graph::IsGraphConnected() const {
-//   s21::s21_Graph result;
-//     for(int i = //     for (int j = 0; j < size_; j++) {
-//       if(result.AdjacencyMatrix_[i][j] !=result.AdjacencyMatrix_[j][i]) {
-//         result.AdjacencyMatrix_[j][i] = result.AdjacencyMatrix_[i][j];
-//       } 
-//     }
-//   }0; i < size_; i++) {
-
-//   std::vector<int> visited(size_, 0);
-//   visited = result.FindPath(result, 0);
-//     // ��������� ����� � ������� �� ������ �������
-//     // ���������, ���� �� ��� ������� ��������
-//     for (int isVisited : visited) {
-//         if (isVisited == 0) {
-//             return false;  // ���� �� �������
-//         }
-//     }
-//     return true;  // ���� �������
-// }
-
 /// @brief Метод, загружающий матрицу в файл
 /// @param const std::string& filename - входящее имя файла, в формате "строка"
 /// @return  no
