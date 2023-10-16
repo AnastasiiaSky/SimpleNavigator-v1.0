a7d406c50ecfaf376c4134aa2d6e1a01a4022cd2
qylenett <nastya.gaponova@bk.ru>
2023-10-16 11:42:00 +0300

    Изменен мктод преобразующий направленную матрицу в ненаправленную

--- src/s21_algorithms/s21_graph_algorithms.cc
+++ src/s21_algorithms/s21_graph_algorithms.cc
@@ -531,11 +531,12 @@ std::vector<std::vector<int>> s21::GraphAlgorithms::ConvertToUndirected(
     const matrix graph_matrix) {
   int n = graph_matrix.size();
   matrix undirected_adj_matrix(n, std::vector<int>(n, 0));
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      if (graph_matrix[i][j] != 0) {
-        undirected_adj_matrix[i][j] = undirected_adj_matrix[j][i] =
-            graph_matrix[i][j];
+  for (int i = 0; i <  graph_matrix.size(); ++i) {
+    for (int j = i + 1; j <  graph_matrix.size(); ++j) {
+      if ( graph_matrix[i][j] != 0 ||  graph_matrix[j][i] != 0) {
+        int weight = std::max( graph_matrix[i][j],  graph_matrix[j][i]);
+        undirected_adj_matrix[i][j] = weight;
+        undirected_adj_matrix[j][i] = weight;
       }
     }
   }
